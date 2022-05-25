38fe6bc39622dfe385ce4432ef93c8605c488baf
Diego Villalobos <diegovilalobos397@gmail.com>
2022-05-25 23:50:05 +0200

    Add anchor to clippy to /feedback

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -3,6 +3,7 @@ import styles from './Home.module.scss'
 export default function Home() {
   return (
     <div className={styles.root}>
+      <img className={styles.dog} src={'/dog.gif'}/>
       <div className={styles.congratulationsContainer}>
         <img className={styles.congratulations} src={'/congratulations.gif'}/>
         <h1 className={styles.title}>
@@ -12,11 +13,12 @@ export default function Home() {
       <h2 className={`${styles.h2} ${styles.github}`}>
         <a className={styles.link} href={'https://github.com/DiegoVillalobosFlores'}>Github</a>
       </h2>
-      <img className={styles.clippy} src={'/clippy.gif'}/>
-      <img className={styles.dog} src={'/dog.gif'}/>
       <div className={styles.carContainer}>
         <img className={styles.car} src={'/car.gif'}/>
       </div>
+      <a href={'/feedback'} className={styles.clippy}>
+        <img src={'/clippy.gif'}/>
+      </a>
     </div>
   )
 }
