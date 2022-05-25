0ef1f168cf9aebc7017fe2ca7d06ad1f98b0dc33
Diego Villalobos <diegovilalobos397@gmail.com>
2022-05-25 23:48:49 +0200

    Add Feedback component

---
+++ src/components/Feedback/Feedback.tsx
@@ -0,0 +1,12 @@
+import styles from './Feedback.module.scss'
+
+export default function Feedback() {
+  return (
+    <div className={styles.root}>
+      <label className={styles.label}>Please let me know what you think, your opinion matters to me!</label>
+      <div>
+        <textarea/>
+      </div>
+    </div>
+  )
+}
